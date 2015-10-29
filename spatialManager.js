a3e1a981a6ba202ffd0af4a69ee4e8b35ab7e3be
andribja <andribja@gmail.com>
2015-10-29 15:14:52 +0000

    asteroids skeleton

---
+++ spatialManager.js
@@ -0,0 +1,75 @@
+/*
+
+spatialManager.js
+
+A module which handles spatial lookup, as required for...
+e.g. general collision detection.
+
+*/
+
+"use strict";
+
+/* jshint browser: true, devel: true, globalstrict: true */
+
+/*
+0        1         2         3         4         5         6         7         8
+12345678901234567890123456789012345678901234567890123456789012345678901234567890
+*/
+
+var spatialManager = {
+
+// "PRIVATE" DATA
+
+_nextSpatialID : 1, // make all valid IDs non-falsey (i.e. don't start at 0)
+
+_entities : [],
+
+// "PRIVATE" METHODS
+//
+// <none yet>
+
+
+// PUBLIC METHODS
+
+getNewSpatialID : function() {
+
+    // TODO: YOUR STUFF HERE!
+    
+    var retval = this._nextSpatialID;
+    this._nextSpatialID++;
+    return retval;
+},
+
+register: function(entity) {
+    var pos = entity.getPos();
+    var spatialID = entity.getSpatialID();
+    
+    // TODO: YOUR STUFF HERE!
+    this._entities[spatialID] = entity;
+},
+
+unregister: function(entity) {
+    var spatialID = entity.getSpatialID();
+
+    // TODO: YOUR STUFF HERE!
+
+},
+
+findEntityInRange: function(posX, posY, radius) {
+
+    // TODO: YOUR STUFF HERE!
+
+},
+
+render: function(ctx) {
+    var oldStyle = ctx.strokeStyle;
+    ctx.strokeStyle = "red";
+    
+    for (var ID in this._entities) {
+        var e = this._entities[ID];
+        util.strokeCircle(ctx, e.posX, e.posY, e.radius);
+    }
+    ctx.strokeStyle = oldStyle;
+}
+
+}
