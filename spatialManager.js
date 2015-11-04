c0e1e472452d9e77680d22318d9e8f55ad83d871
unknown <danielgretars@bitbucket.org>
2015-11-04 15:51:08 +0000

    Added implementation from asteroids

--- spatialManager.js
+++ spatialManager.js
@@ -22,7 +22,7 @@ var spatialManager = {
 
 _nextSpatialID : 1, // make all valid IDs non-falsey (i.e. don't start at 0)
 
-_entities : [],
+_entities : {},
 
 // "PRIVATE" METHODS
 //
@@ -32,42 +32,44 @@ _entities : [],
 // PUBLIC METHODS
 
 getNewSpatialID : function() {
-
-    // TODO: YOUR STUFF HERE!
     
-    var retval = this._nextSpatialID;
-    this._nextSpatialID++;
-    return retval;
+    return this._nextSpatialID++;
 },
 
 register: function(entity) {
-    var pos = entity.getPos();
     var spatialID = entity.getSpatialID();
-    
-    // TODO: YOUR STUFF HERE!
+
     this._entities[spatialID] = entity;
 },
 
 unregister: function(entity) {
     var spatialID = entity.getSpatialID();
 
-    // TODO: YOUR STUFF HERE!
-
+    delete this._entities[spatialID];
 },
 
 findEntityInRange: function(posX, posY, radius) {
 
-    // TODO: YOUR STUFF HERE!
+    for(var ID in this._entities) {
+        var collidingEntity = this._entities[ID];
+        var pos = collidingEntity.getPos();
+        var distSq = util.distSq(posX, posY, pos.posX, pos.posY);
+
+        if(distSq <= util.square(radius + collidingEntity.getRadius())) {
+            return collidingEntity;
+        }
+    }
 
 },
 
 render: function(ctx) {
     var oldStyle = ctx.strokeStyle;
     ctx.strokeStyle = "red";
-    
+
     for (var ID in this._entities) {
         var e = this._entities[ID];
-        util.strokeCircle(ctx, e.posX, e.posY, e.radius);
+        
+        util.strokeCircle(ctx, e.cx, e.cy, e.getRadius());
     }
     ctx.strokeStyle = oldStyle;
 }
