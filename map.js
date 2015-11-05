845dc3143d4c27f3eeb5214c5ffbc91d4b6dff7e
andribja <andribja@gmail.com>
2015-11-05 14:42:36 +0000

    Added check for vertical / horizontal line collision with map

--- map.js
+++ map.js
@@ -17,13 +17,13 @@ Map.prototype.isLand = function(x, y) {
 }
 
 Map.prototype.getAlphaAt = function(x, y) {
-    var i =  y * g_canvas.width * 4 + x * 4 + 3;
+    var i =  parseInt(y) * g_canvas.width * 4 + parseInt(x) * 4 + 3;
 
     return this.imageData.data[i];
 };
 
 Map.prototype.setAlphaAt = function(x, y, alpha) {
-    var i =  y * g_canvas.width * 4 + x * 4 + 3;
+    var i =  parseInt(y) * g_canvas.width * 4 + parseInt(x) * 4 + 3;
     
     this.imageData.data[i] = alpha;
 };
@@ -32,12 +32,28 @@ Map.prototype.render = function(ctx) {
     ctx.putImageData(this.imageData, 0, 0);
 };
 
-Map.prototype.collidesWithMap = function(cx, cy, r) {
+Map.prototype.circleCollidesWithMap = function(cx, cy, r) {
     var theta;
     for(theta = 0; theta < 2*Math.PI; theta += Math.PI/12) {
         var y = r*Math.sin(theta);
         var x = r*Math.cos(theta);
         if(this.isLand(x,y)) return true;
+    }
+    return false;
+}
+
+Map.prototype.vertLineCollidesWithMap = function(x, y1, y2) {
+    var y;
+    for(y = Math.min(y1,y2); y < Math.max(y1,y2); y++) {
+        if(this.isLand(x, y)) return true;
+    }
+    return false;
+}
+
+Map.prototype.horiLineCollidesWithMap = function(x1, x2, y) {
+    var x;
+    for(x = Math.min(x1,x2); x < Math.max(x1,x2); x++) {
+        if(this.isLand(x, y)) return true;
     }
     return false;
 }
